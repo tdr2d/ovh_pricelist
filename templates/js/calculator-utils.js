5b574f9f3f318c97e9022d117f104558661fb95f
tdr2d <28861767+tdr2d@users.noreply.github.com>
2023-08-07 22:17:27 -0400

    finished DeserializeURI

--- templates/js/calculator-utils.js
+++ templates/js/calculator-utils.js
@@ -1,12 +1,16 @@
 
-function getDefaultItem(key, quantity, commit, discount) {
-    return {
+function getItem(key, quantity, commit, discount) {
+    console.log(res[key]);
+    let item = {
         key: key,
         description: res[key].description,
         setupfee: res[key].setupfee || 0,
         pricePerUnit: pricekey in res[key] ? res[key][pricekey] : res[key].price,
         quantity: quantity || 1,
         commit: commit || 1,
-        discout: discount || 0,
+        discount: discount || 0,
     };
+    item['setupfee'] *= item.quantity;
+    item['total'] = item['pricePerUnit'] * item.quantity * (1 - (item.discount) / 100);
+    return item;
 }
--- templates/js/serialize.js
+++ templates/js/serialize.js
@@ -9,10 +9,12 @@ const ABREV_TOTAL = {
 const ABREV_ITEM = {
     'key': 'k',
     'commit': 'c',
-    'discout': 'e',
+    'discount': 'e',
     'quantity': 'q',
-    'setupfee': 's',
 }
+
+const ZONE_RE = RegExp('z([A-Z ]{3,})', 'g');
+const ITEM_RE = RegExp('k([pqb]-[a-zA-Z0-9]{2})', 'g');
 
 function SerilizeState() {
     let output = `${location.origin}${location.pathname}?con=${conformity}&sub=${sub}`;
@@ -44,40 +46,35 @@ function buildReverseDict(dict) {
     return reverse_dict;
 }
 
-const ZONE_RE = RegExp('^[A-Z ]{3,}', 'g');
-
 function DeserializeURI(uri) {
-    let reverse_abrev_item = buildReverseDict(ABREV_ITEM);
     let reverse_abrev_total = buildReverseDict(ABREV_TOTAL);
-    const search = '?' + uri.split('?')[1]; // location.search
+    let search = '?' + uri.split('?')[1]; // location.search
 
-    let rstate = {};
+    let newstate = {zones: []};
     Object.keys(reverse_abrev_total).forEach((k) => {
-        rstate[reverse_abrev_total[k]] = decodeURIComponent(new URLSearchParams(search).get(k));
+        newstate[reverse_abrev_total[k]] = decodeURIComponent(new URLSearchParams(search).get(k));
     });
     
-    let zones = {};
-    search = new URLSearchParams(search).get('zs');
+    search = decodeURIComponent(new URLSearchParams(search).get('zs'));
+    const search_zones = search.split(ZONE_RE).slice(1); // Split zones
+    for (let i = 0; i < search_zones.length; i+=2) {
+        const zoneData = search_zones[i+1].split(ITEM_RE).slice(1);
+        let items = []
+        for (let j=0; j < zoneData.length; j+=2) {  // Split items
+            const key = zoneData[j];
+            const data = zoneData[j+1];
+            const qcd = ['quantity', 'commit', 'discount'].map((x) => {
+                return data.match(new RegExp(`${ABREV_ITEM[x]}[0-9]{1,4}`, 'g'))[0];
+            });
+            items.push(getItem(key, qcd[0], qcd[1], qcd[2]));
+        }
+        newstate.zones.push({'key': search_zones[i], 'items': items})
+    }
 
-    // Split zones
-    const search_zones = search.replace(/(.)z([A-Z]{3,})/g, (m, p1, p2) => `${p1}\n${p2}`).split('\n');
-    search_zones[0] = search_zones[0].slice(1);
-
-
-
-    search_zones.map(z => {
-        // Split Items
-        zoneKey = z.match(ZONE_RE)[0];
-        z = z.replace(ZONE_RE, '');
-        items = z.replace(/k[pqb]-[a-z]{2}/g, '\n$&').split('\n')
-        // TODO
-    })
-
-
-    // decodeURIComponent(search_zones[0]).match(/([A-Z ]{3,})k([pqb]-[a-z]{2})/)
-    console.log(rstate);
+    console.log(newstate);
+    return newstate;
 }
 
 // TEST
-// DeserializeURI('http://localhost/calculator.html?con=default&sub=FR&t=mondevi%20pour%20client%2011&b=13098%20%26%20akdfj%20%2F%2F&a=asdfj&s=b&u=0&zs=zSBGkb-aac1e0q1s135.99')
-DeserializeURI('http://localhost/calculator.html?con=default&sub=FR&t=&b=&a=&s=b&u=0&zs=zSBGkb-aac1e0q1s135.99kp-aac1e0q1s0zWAWkb-aoc1e0q1s99.99kq-aac1e0q1s0zSBG%20TZkq-abc1e0q1s0kb-a6c1e0q1s1011.99');
+// DeserializeURI('http://localhost/calculator.html?con=default&sub=FR&t=mondevi%20pour%20client%2011&b=13098%20%26%20akdfj%20%2F%2F&a=asdfj&s=b&u=0&zs=zSBGkb-aac1e0q1')
+// DeserializeURI('http://localhost/calculator.html?con=default&sub=FR&t=&b=&a=&s=b&u=0&zs=zSBGkb-aac1e0q1kp-aac1e0q1zWAWkb-aoc1e0q1kq-aac1e0q1zSBG%20TZkq-abc1e0q1kb-a6c1e0q1');
